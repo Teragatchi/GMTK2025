7d0a36e5b848cdaaecfd6542860012f775f76686
Teragatchi <Trishagup12@gmail.com>
2025-08-01 19:26:58 +0400

    added half of eating mechanic (will add rest after dinner)

---
+++ GMTK2025LoopGame/Assets/Scripts/PlayerThreadInventory.cs
@@ -0,0 +1,29 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerThreadInventory : MonoBehaviour
+{
+    private Dictionary<Color, float> threadInventory = new();
+
+    public void AddThread(Color color, float amount)
+    {
+        if (threadInventory.ContainsKey(color))
+            threadInventory[color] += amount;
+        else
+            threadInventory[color] = amount;
+
+        //UIThreadDisplay.Instance.UpdateColor(color, threadInventory[color]);
+    }
+
+    public bool UseThread(Color color, float amount)
+    {
+        if (!threadInventory.ContainsKey(color) || threadInventory[color] < amount)
+            return false;
+
+        threadInventory[color] -= amount;
+        //UIThreadDisplay.Instance.UpdateColor(color, threadInventory[color]);
+        return true;
+    }
+
+    public float GetThreadAmount(Color color) => threadInventory.ContainsKey(color) ? threadInventory[color] : 0f;
+}
